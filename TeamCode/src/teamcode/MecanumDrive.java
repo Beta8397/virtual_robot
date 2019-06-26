6929b4a038220e3169d56a43497c44d693e009f8
Alan Smith <alan@randomsmiths.com>
2019-06-26 19:42:37 -0400

    Add the Mecanum Drive OpMode from FTC16072

---
+++ TeamCode/src/teamcode/MecanumDrive.java
@@ -0,0 +1,54 @@
+package teamcode;
+
+import virtual_robot.hardware.*;
+
+class MecanumDrive {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+    private DcMotor backLeft;
+
+    void init(HardwareMap hwMap) {
+        frontLeft = hwMap.get(DcMotor.class, "front_left_motor");
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight = hwMap.get(DcMotor.class, "front_right_motor");
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft = hwMap.get(DcMotor.class, "back_left_motor");
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight = hwMap.get(DcMotor.class, "back_right_motor");
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    void setSpeeds(double flSpeed, double frSpeed, double blSpeed, double brSpeed) {
+        double largest = 1.0;
+        largest = Math.max(largest, Math.abs(flSpeed));
+        largest = Math.max(largest, Math.abs(frSpeed));
+        largest = Math.max(largest, Math.abs(blSpeed));
+        largest = Math.max(largest, Math.abs(brSpeed));
+
+        frontLeft.setPower(flSpeed / largest);
+        frontRight.setPower(frSpeed / largest);
+        backLeft.setPower(blSpeed / largest);
+        backRight.setPower(brSpeed / largest);
+    }
+
+    void driveMecanum(double forward, double strafe, double rotate) {
+        double frontLeftSpeed = forward + strafe + rotate;
+        double frontRightSpeed = forward - strafe - rotate;
+        double backLeftSpeed = forward - strafe + rotate;
+        double backRightSpeed = forward + strafe - rotate;
+
+        setSpeeds(frontLeftSpeed, frontRightSpeed, backLeftSpeed, backRightSpeed);
+    }
+
+    void reportEncoders(Telemetry telemetry) {
+        telemetry.addData("Encoders", "%d %d %d %d",
+                frontLeft.getCurrentPosition(),
+                frontRight.getCurrentPosition(),
+                backLeft.getCurrentPosition(),
+                backRight.getCurrentPosition());
+    }
+}
