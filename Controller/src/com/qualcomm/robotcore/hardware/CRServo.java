9acdfb9b080751947849a6b3cbb574f27012d055
James Kenney <jkenney2@charter.net>
2020-06-03 15:17:06 -0400

    latest commit

--- Controller/src/com/qualcomm/robotcore/hardware/CRServo.java
+++
@@ -1,8 +0,0 @@
-package com.qualcomm.robotcore.hardware;
-
-/**
- * CRServo interface included for compatibility with FTC SDK. All of its functionality is found in the
- * DcMotorSimple interface.
- */
-public interface CRServo extends DcMotorSimple {
-}
--- Controller/src/com/qualcomm/robotcore/hardware/HardwareDevice.java
+++
@@ -1,7 +0,0 @@
-package com.qualcomm.robotcore.hardware;
-
-/**
- * Interface to represent any hardware device that can be managed by HardwareMap.
- */
-public interface HardwareDevice {
-}
