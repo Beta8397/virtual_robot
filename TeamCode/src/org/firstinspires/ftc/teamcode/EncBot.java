5a6ac0773dd8f8c9c4dbf28aeae1b50ee3807505
James_Kenney <jkenney2@charter.net>
2020-07-25 19:01:33 -0400

    Fixed typo in EncBot.java

--- TeamCode/src/org/firstinspires/ftc/teamcode/EncBot.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/EncBot.java
@@ -49,7 +49,8 @@ public class EncBot {
 
     public void resetOdometry(double x, double y, double headingRadians){
         pose[0] = x;
-        pose[1] = 1;
+        //oops
+        pose[1] = y;
         pose[2] = headingRadians;
         for (int i=0; i<3; i++) prevTicks[i] = encoders[i].getCurrentPosition();
     }
