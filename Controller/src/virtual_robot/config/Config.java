3fe15a8bea60faa67f90fd0ca0e3a71722aeed8d
James Kenney <jkenney2@charter.net>
2020-09-12 20:29:34 -0400

    Switched to ultimate goal field

--- Controller/src/virtual_robot/config/Config.java
+++ Controller/src/virtual_robot/config/Config.java
@@ -20,7 +20,7 @@ public class Config {
     /**
      * The image object for the field.
      */
-    public static final Image BACKGROUND = new Image("/virtual_robot/assets/skystone_field648.bmp");
+    public static final Image BACKGROUND = new Image("/virtual_robot/assets/ultimate_goal_648.bmp");
 
     /**
      * If true, Virtual Gamepad joysticks and triggers will stay in the position where they were
