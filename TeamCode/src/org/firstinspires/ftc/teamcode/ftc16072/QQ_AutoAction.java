cd6bd3a66cff44d73ad1db8d1230794ecdbb2b29
Keerthana Thirukonda <keerthana.g.thirukonda@gmail.com>
2021-10-09 17:33:28 -0400

    deleting last year's code

--- TeamCode/src/org/firstinspires/ftc/teamcode/ftc16072/QQ_AutoAction.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.ftc16072;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-
-abstract class QQ_AutoAction {
-    abstract boolean run(Robot robot, double gameTime, Telemetry telemetry);
-}
