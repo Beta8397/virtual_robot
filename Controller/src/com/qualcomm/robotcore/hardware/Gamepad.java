b6d0b75eb68becdeaa6711230d57f99ddfa969e6
Alan Smith <alan@randomsmiths.com>
2022-08-20 18:22:52 -0400

    Add LED commands to Gamepad class

--- Controller/src/com/qualcomm/robotcore/hardware/Gamepad.java
+++ Controller/src/com/qualcomm/robotcore/hardware/Gamepad.java
@@ -34,7 +34,14 @@ Modified by Virtual_Robot contributors
 
 package com.qualcomm.robotcore.hardware;
 
+import com.qualcomm.robotcore.util.Range;
 import com.studiohartman.jamepad.ControllerState;
+
+import org.firstinspires.ftc.robotcore.internal.collections.EvictingBlockingQueue;
+
+import java.util.ArrayList;
+import java.util.concurrent.ArrayBlockingQueue;
+
 import virtual_robot.controller.VirtualGamePadController;
 
 /**
@@ -172,7 +179,7 @@ public class Gamepad {
      * Should allow a team to program with whatever controllers they prefer, but
      * be able swap controllers easily without changing code.
      */
-    protected void updateButtonAliases(){
+    protected void updateButtonAliases() {
         // There is no assignment for touchpad because there is no equivalent on XBOX controllers.
         circle = b;
         cross = a;
@@ -183,4 +190,123 @@ public class Gamepad {
         ps = guide;
     }
 
+    // To prevent blowing up the command queue if the user tries to send an LED command in a tight loop,
+    // we have a 1-element evicting blocking queue for the outgoing LED effect and the event loop periodically
+    // just grabs the effect out of it (if any)
+    public EvictingBlockingQueue<LedEffect> ledQueue = new EvictingBlockingQueue<>(new ArrayBlockingQueue<LedEffect>(1));
+
+    public static final int ID_UNASSOCIATED = -1;
+    protected byte userForEffects = ID_UNASSOCIATED;
+
+    public void setUserForEffects(byte userForEffects) {
+        this.userForEffects = userForEffects;
+    }
+
+    public static final int LED_DURATION_CONTINUOUS = -1;
+
+    public static class LedEffect {
+        public static class Step {
+            public int r;
+            public int g;
+            public int b;
+            public int duration;
+        }
+
+        public final ArrayList<Step> steps;
+        public final boolean repeating;
+        public int user;
+
+        private LedEffect(ArrayList<Step> steps, boolean repeating) {
+            this.steps = steps;
+            this.repeating = repeating;
+        }
+
+        public static class Builder {
+            private ArrayList<Step> steps = new ArrayList<>();
+            private boolean repeating;
+
+            /**
+             * Add a "step" to this LED effect. A step basically just means to set
+             * the LED to a certain color (r,g,b) for a certain duration. By creating a chain of
+             * steps, you can create unique effects.
+             *
+             * @param r the red LED intensity (0.0 - 1.0)
+             * @param g the green LED intensity (0.0 - 1.0)
+             * @param b the blue LED intensity (0.0 - 1.0)
+             * @return the builder object, to follow the builder pattern
+             */
+            public Builder addStep(double r, double g, double b, int durationMs) {
+                return addStepInternal(r, g, b, Math.max(durationMs, 0));
+            }
+
+            /**
+             * Set whether this LED effect should loop after finishing,
+             * unless the LED is otherwise commanded differently.
+             *
+             * @param repeating whether to loop
+             * @return the builder object, to follow the builder pattern
+             */
+            public Builder setRepeating(boolean repeating) {
+                this.repeating = repeating;
+                return this;
+            }
+
+            private Builder addStepInternal(double r, double g, double b, int duration) {
+
+                r = Range.clip(r, 0, 1);
+                g = Range.clip(g, 0, 1);
+                b = Range.clip(b, 0, 1);
+
+                Step step = new Step();
+                step.r = (int) Math.round(Range.scale(r, 0.0, 1.0, 0, 255));
+                step.g = (int) Math.round(Range.scale(g, 0.0, 1.0, 0, 255));
+                step.b = (int) Math.round(Range.scale(b, 0.0, 1.0, 0, 255));
+                step.duration = duration;
+                steps.add(step);
+                return this;
+            }
+
+            /**
+             * After you've added your steps, call this to get an LedEffect object
+             * that you can then pass to {@link #runLedEffect(LedEffect)}
+             *
+             * @return an LedEffect object, built from previously added steps
+             */
+            public LedEffect build() {
+                return new LedEffect(steps, repeating);
+            }
+        }
+    }
+
+    public void setLedColor(double r, double g, double b, int durationMs) {
+
+        if (durationMs != LED_DURATION_CONTINUOUS) {
+            durationMs = Math.max(0, durationMs);
+        }
+
+        LedEffect effect = new LedEffect.Builder().addStepInternal(r, g, b, durationMs).build();
+        queueEffect(effect);
+    }
+
+    /**
+     * Run an LED effect built using {@link LedEffect.Builder}
+     * The LED effect will be run asynchronously; your OpMode will
+     * not halt execution while the effect is running.
+     * <p>
+     * Calling this will displace any currently running LED effect
+     */
+    public void runLedEffect(LedEffect effect) {
+        queueEffect(effect);
+    }
+
+    private void queueEffect(LedEffect effect) {
+        // We need to make a new object here, because since the effect is queued and
+        // not set immediately, if you called this function for two different gamepads
+        // but passed in the same instance of an LED effect object, then it could happen
+        // that both effect commands would be directed to the *same* gamepad. (Because of
+        // the "user" field being modified before the queued command was serialized)
+        LedEffect copy = new LedEffect(effect.steps, effect.repeating);
+        copy.user = userForEffects;
+        ledQueue.offer(copy);
+    }
 }
