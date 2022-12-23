36bbfc6764635787be0af6e9dc74e8c994cd1ef8
Alan Smith <alan@randomsmiths.com>
2022-12-23 17:13:45 -0500

    Adding things so April Tags and OpenCV will build (but not run correctly)

---
+++ Controller/src/com/acmerobotics/dashboard/FtcDashboard.java
@@ -0,0 +1,16 @@
+package com.acmerobotics.dashboard;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+
+public class FtcDashboard {
+    private static FtcDashboard instance;
+
+    public static FtcDashboard getInstance() {
+        return instance;
+    }
+
+    public void sendTelemetryPacket(TelemetryPacket telemetryPacket) {
+
+
+    }
+}
---
+++ Controller/src/com/acmerobotics/dashboard/telemetry/TelemetryPacket.java
@@ -0,0 +1,18 @@
+package com.acmerobotics.dashboard.telemetry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class TelemetryPacket {
+    /**
+     * Creates a new telemetry packet.
+     */
+    public TelemetryPacket() {
+    }
+
+    public void put(String key, Object value) {
+
+    }
+}
