81bf0ec3114f3ef4b5bb6b8f9d6358146bf5a176
Kevin Furlong <kevinmfurlong@gmail.com>
2023-10-17 17:09:39 -0400

    Started on L4 added blank for L5

---
+++ TeamCode/src/org/firstinspires/ftc/teamcode/a4_Conditions.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * In our programs, we need to do different things based on gamepad input, sensor
+ * values, timers, and robot states. To do this, we can use conditions with
+ * if-else if-else blocks.
+ *
+ * First lets talk about if statements. An if-else if-else block looks like this:
+ *
+ * if(condition){
+ *     //run the code here if the condition is true
+ *     // You can use an if statement by itself - it works just like having an
+ *     // empty else.
+ * }
+ * else if(condition 2){
+ *     //run the code here if condition 1 is false and condition 2 is true
+ *     //you can include
+ * }
+ * else{
+ *     //run this code if conditions 1 & 2 are both false
+ * }
+ *
+ */
+@TeleOp(name = "Lesson 4: Conditions")
+public class a4_Conditions implements LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+        while(opModeIsActive()){
+
+        }
+    }
+}
