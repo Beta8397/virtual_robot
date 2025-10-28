d430aeea5a0c6ae66ff8285f71c190b6851cbbb2
James Kenney <jkenney2@charter.net>
2025-10-27 21:37:19 -0400

    Deleted timberbots code

--- TeamCode/src/org/firstinspires/ftc/teamcode/pathutil/ParametricFunction.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.pathutil;
-
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-
-public interface ParametricFunction {
-    float p(float s);
-    float d1(float s);
-    float d2(float s);
-}
--- TeamCode/src/org/firstinspires/ftc/teamcode/pathutil/ParametricFunction2D.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.pathutil;
-
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
-
-public interface ParametricFunction2D {
-    VectorF p(float s);
-    VectorF d1(float s);
-    VectorF d2(float s);
-}
